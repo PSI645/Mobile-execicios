cdfc933988d2c3a391e4b01cc0136db0a8c31ae6
unknown <3bmod@e041edu.local>
2026-03-26 22:18:34 -0300

    26/03/2026 Falta coisas da aula 25/03/26

---
+++ app/src/main/java/com/example/constraintlayout/model/Usuario.kt
@@ -0,0 +1,9 @@
+package com.example.constraintlayout.model
+
+data class Usuario {
+    val id: Long,
+    val nome: String,
+    val email: String,
+    val telefone: String,
+    val senha: String
+}
---
+++ app/src/main/java/com/example/constraintlayout/network/ApiService.kt
@@ -0,0 +1,4 @@
+package com.example.constraintlayout.network
+
+interface ApiService {
+}
