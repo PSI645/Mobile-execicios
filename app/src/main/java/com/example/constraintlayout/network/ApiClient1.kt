4471acf43c440a0ee6f6c54a846b48f9e3dc47a4
unknown <3bmod@e041edu.local>
2026-04-08 22:22:39 -0300

    Ultima modificação

--- app/src/main/java/com/example/constraintlayout/network/ApiClient1.kt
+++ app/src/main/java/com/example/constraintlayout/network/ApiClient1.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.net.URL
 
 object ApiClient1 {
-    private val URL = "https:/192.168.10.241:8080/"
+    private val URL = "http:/192.168.10.241:8080/"
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
