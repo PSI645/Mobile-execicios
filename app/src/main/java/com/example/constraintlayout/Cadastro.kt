6c80f96e9617c4cd4e9d4b70eaf1a08617d0d3e6
unknown <3bmod@e041edu.local>
2026-04-08 22:16:44 -0300

    modificações do visual e adição da função cadastro main

--- app/src/main/java/com/example/constraintlayout/Cadastro.kt
+++ app/src/main/java/com/example/constraintlayout/Cadastro.kt
@@ -3,12 +3,16 @@ package com.example.constraintlayout
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.constraintlayout.model.Usuario
 import com.example.constraintlayout.network.ApiClient1
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class Cadastro : AppCompatActivity() {
@@ -30,9 +34,9 @@ class Cadastro : AppCompatActivity() {
         val email = findViewById<EditText>(R.id.editTextEmail)
         val senha = findViewById<EditText>(R.id.editTextSenha)
 
-        var btnCadastro = findViewById<Button>(R.id.btnCadastrar)
-        
-        btnCadastro.setOnClickListener {
+        var btnCadastrar = findViewById<Button>(R.id.btnCadastrar)
+
+        btnCadastrar.setOnClickListener {
             var usuario = Usuario(
                 nome = nome.text.toString(),
                 email = email.text.toString(),
@@ -40,7 +44,40 @@ class Cadastro : AppCompatActivity() {
                 senha = senha.text.toString(),
                 cpf = cpf.text.toString()
             )
-        }
 
+            fazerCadastro(usuario)
+
+        }
     }
+    fun fazerCadastro(usuario: Usuario) {
+        apiService.criarUsuario(usuario).enqueue(object : Callback<Usuario> {
+
+            override fun onResponse(call: Call<Usuario>, response: Response<Usuario>) {
+                if (response.isSuccessful) {
+                    Toast.makeText(
+                        this@Cadastro,
+                        "Cadastrado com Sucesso",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                } else {
+                    Toast.makeText(
+                        this@Cadastro,
+                        "Erro: ${response.message()}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onFailure(call: Call<Usuario>, t: Throwable) {
+                Toast.makeText(
+                    this@Cadastro,
+                    "Falha na rede: ${t.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+                println(t.message)
+            }
+        })
+    }
+
 }
--- app/src/main/java/com/example/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.constraintlayout
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val button = findViewById<Button>(R.id.buttonCadastro)
+
+        button.setOnClickListener {
+           val x=Intent(this,Cadastro:: class.java)
+            startActivity(x)
         }
     }
 }
